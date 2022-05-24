6fe5014c786dccc8cf993f6357867cac631c0a3c
loremod <lorenzomod@hotmail.it>
2022-05-24 19:16:11 +0200

    integrating translation with multiple containers execution

--- clientTCP/app/main.py
+++ clientTCP/app/main.py
@@ -1,9 +1,13 @@
+import sys
 from tgScraper import *
 from translator import translator
 
-if __name__ == '__main__': 
- 
 
+
+
+if __name__ == '__main__': 
+    channel = "https://t.me/" + sys.argv[1]
+    last_id = sys.argv[2]
 
     ## language
     # input languages
@@ -15,7 +19,7 @@ if __name__ == '__main__':
     tr = translator(file="translators.txt",FROM = "it", TO= "en")
 
     # fe = tgFetch("t.me/ciao")
-    fetcher = tgScraper("https://t.me/UkraineNowItalian","logstash",10155,tr)
+    fetcher = tgScraper(channel,"logstash",10155,tr)
     # fetcher = tgScraper("https://t.me/UkraineNow","logstash",10155,tr)
     # fetcher = tgScraper("t.me/InteressanteTelegramChannel","logstash",10155,tr)
     russian_text = """
@@ -24,36 +28,6 @@ if __name__ == '__main__':
      Его шерсть была средней длинны и коричневого цвета. Родители купили большую клетку для него, с двумя этажами. Я был очень рад, когда у меня появился маленький друг.
      Было очень весело смотреть как Хома бегает в колесе. Мне нравилось кормить его морковкой и орехами."""
 
-#  'I want to learn to play guitar and piano.\nI wanted to have a dog since I was a kid, but my parents were not allowed. 
-# While I was a child, I had a hamster Homa. Homa was very small and fluffy. His wool was medium long and brown. Parents bought a large cage for him, with two floors. 
-# I was very happy to have a little friend.\nIt was very fun to watch Homa running in the wheel. I liked to feed him carrots and nuts.'
-
-
-    russian_text2 = """Новая Зеландия выделит инструкторов для обучения украинских артиллеристов.
-
-Премьер-министр Новой Зеландии Джасинда Ардерн заявила, что руководство новозеландской армии направит в Украину 30 военных инструкторов для обучения украинских военных работе с легкой артиллерийской установкой L119.
-
-Кроме того, Украине передадут амуницию, артиллерийское оборудование и системы наведения в количестве 40 штук."""
-    uk_text2 = """
-     МОЗ повідомляє: \nМОЗ та Uber домовилися про безкоштовне перевезення лікарів на час карантину
-    За домовленістю з Міністерством охорони здоров’я України та органами місцевого самоврядування, Uber буде безкоштовно доставляти до лікарень медичних працівників, задіяних у запобіганні та ліквідації коронавірусної інфекції в Україні. Ця домовленість включає 10 000 поїздок, які медичні працівники можуть здійснити, щоб дістатися до своїх лікарень та робочих місць. 
-    Ця опція буде доступною в містах, де функціонує сервіс Uber: Київ, Одеса, Львів, Дніпро, Харків, Запоріжжя та Вінниця.
-    МОЗ України вдячне Uber за виявлену соціальну відповідальність та закликає інші транспортні сервіси долучитися до ініціативи та допомогти дістатися на роботу тим, хто рятує людські життя.'}
-    
-    """
-    # html_text = """
-    # <html>
-    #     <head>Ciao</head>
-    #     <body>
-    #         Sono un essere umano
-    #         <div>
-    #             <p>Questo è un paragrafo</p>
-    #         </div>
-    #     </body>
-    # </html>
-    # """
-
-    
     # print(tr.list)
     # print(translator.translate(text="Я хочу научиться играть на гитаре и фортепиано", FROM="ru", TO="en", translator="argos"))
     # print(translator.translate(text="Я хочу научиться играть на гитаре и фортепиано", FROM="ru", TO="en", translator="tencent"))
@@ -78,8 +52,8 @@ if __name__ == '__main__':
 
 
     #Ottiene tutti i messaggi da un minimo ad un massimo
-    # fetcher.getAllMessages(min_id=127,max_id=150,sendTCP=True)
-    # fetcher.getAllMessages(min_id=127,max_id=250,sendTCP=True)
+    # fetcher.getAllMessages(min_id=last_id,max_id=150,sendTCP=True)
+    # fetcher.getAllMessages(min_id=last_id,max_id=250,sendTCP=True)
     # fetcher.getAllMessages(min_id=11600, sendTCP=True)
     # fetcher.getAllMessages(max_id=572)
 
