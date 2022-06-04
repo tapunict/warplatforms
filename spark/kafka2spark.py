8bfe9f14c13cfd12a48ceea59710e1ad9b571ba0
loremod <lorenzomod@hotmail.it>
2022-06-04 20:22:31 +0200

    kafka to spark

--- spark/kafka2spark.py
+++ spark/kafka2spark.py
@@ -3,25 +3,36 @@ from __future__ import print_function
 import sys
 
 from pyspark import SparkContext
+from pyspark.sql import SparkSession
 from pyspark.streaming import StreamingContext
-from pyspark.streaming.kafka import KafkaUtils
+from pyspark.sql.dataframe import DataFrame
 
+def elaborate(batch_df: DataFrame, batch_id: int):
+  batch_df.show(truncate=False)
 
-sc = SparkContext(appName="kafka2Spark")
-ssc = StreamingContext(sc, 2)
-
+sc = SparkContext(appName="PythonStructuredStreamsKafka")
+spark = SparkSession(sc)
 sc.setLogLevel("WARN")
-ssc = StreamingContext(sc, 1)
 
-brokers="localhost:9092"
+kafkaServer="localhost:9092"
 topic = "telegram-messages"
 
-kvs = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
-lines = kvs.map(lambda x: x[1])
-counts = lines.flatMap(lambda line: line.split(" ")) \
-    .map(lambda word: (word, 1)) \
-    .reduceByKey(lambda a, b: a+b)
-counts.pprint()
+# Streaming Query
 
-ssc.start()
-ssc.awaitTermination()
+df = spark \
+  .readStream \
+  .format("kafka") \
+  .option("kafka.bootstrap.servers", kafkaServer) \
+  .option("subscribe", topic) \
+  .load()
+
+df.selectExpr("CAST(timestamp AS STRING)","CAST(value AS STRING)") \
+  .writeStream \
+  .foreachBatch(elaborate) \
+  .start() \
+  .awaitTermination()
+
+#df.writeStream \
+#  .format("console") \
+#  .start() \
+#  .awaitTermination()
