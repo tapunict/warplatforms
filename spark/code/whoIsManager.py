f25e7d01cff714d5b7a84a32c36351f2f42209c8
Kederf-8 <napolig05@gmail.com>
2022-06-20 02:18:37 +0200

    pre-exam adjustments pt.2

--- spark/code/whoIsManager.py
+++ spark/code/whoIsManager.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 import time
 import whois
-from pyspark.sql.functions import udf,lit
+from pyspark.sql.functions import udf, lit
 import json
 
-def extractElem(var,pos="Last",shouldBe=""):
+
+def extractElem(var, pos="Last", shouldBe=""):
     index = 0
 
     if shouldBe == "datetime":
@@ -12,7 +13,7 @@ def extractElem(var,pos="Last",shouldBe=""):
             return datetime.min
         return var
 
-    if isinstance(var,list):
+    if isinstance(var, list):
         if pos == "Last":
             index = len(var)-1
         else:
@@ -20,53 +21,60 @@ def extractElem(var,pos="Last",shouldBe=""):
         return var[index]
     return var
 
-url_prove = ["https://www.aranzulla.it","https://www.kmu.gov.ua/en","https://it.wikipedia.org/wiki/Dragon_Ball_Z","https://it.wikipedia.org/wiki/Che_Guevara","https://it.wikipedia.org/wiki/Coordinate_geografiche","https://it.wikipedia.org/wiki/Vladimir_Putin","https://stackoverflow.com/questions/32331848/create-a-custom-transformer-in-pyspark-ml","https://sparkbyexamples.com/pyspark/pyspark-cast-column-type","https://it.quora.com/partners"]
+
+url_prove = ["https://www.aranzulla.it", "https://www.kmu.gov.ua/en", "https://it.wikipedia.org/wiki/Dragon_Ball_Z", "https://it.wikipedia.org/wiki/Che_Guevara", "https://it.wikipedia.org/wiki/Coordinate_geografiche",
+             "https://it.wikipedia.org/wiki/Vladimir_Putin", "https://stackoverflow.com/questions/32331848/create-a-custom-transformer-in-pyspark-ml", "https://sparkbyexamples.com/pyspark/pyspark-cast-column-type", "https://it.quora.com/partners"]
+
 
 class whoIsManager:
 
-    relevant_fields = ["domain_name","whois_server","registrar","org","referral_url","state","city","country","creation_date","updated_date","expiration_date"]
+    relevant_fields = ["domain_name", "whois_server", "registrar", "org", "referral_url",
+                       "state", "city", "country", "creation_date", "updated_date", "expiration_date"]
 
     def __init__(self):
         self.cache = {}
-        
-        self.domain_name = lambda x: extractElem(self.lookFor(x,"domain_name"),pos="Last")
-        self.whois_server = lambda x:self.lookFor(x,"whois_server")
-        self.registrar = lambda x:self.lookFor(x,"registrar")
-        self.org = lambda x:self.lookFor(x,"org")
-        self.referral_url = lambda x:self.lookFor(x,"referral_url")
-        self.state = lambda x:self.lookFor(x,"state")
 
-        self.city = lambda x:self.lookFor(x,"city")
-        self.country = lambda x:self.lookFor(x,"country")
-        
-        self.address = lambda x: extractElem(self.lookFor(x,"address"))
+        self.domain_name = lambda x: extractElem(
+            self.lookFor(x, "domain_name"), pos="Last")
+        self.whois_server = lambda x: self.lookFor(x, "whois_server")
+        self.registrar = lambda x: self.lookFor(x, "registrar")
+        self.org = lambda x: self.lookFor(x, "org")
+        self.referral_url = lambda x: self.lookFor(x, "referral_url")
+        self.state = lambda x: self.lookFor(x, "state")
 
-        self.creation_date = lambda x:extractElem(self.lookFor(x,"creation_date"),shouldBe="datetime")
-        self.updated_date = lambda x: extractElem(self.lookFor(x,"updated_date"),shouldBe="datetime")
-        self.expiration_date = lambda x: extractElem(self.lookFor(x,"expiration_date"),shouldBe="datetime")
-        
+        self.city = lambda x: self.lookFor(x, "city")
+        self.country = lambda x: self.lookFor(x, "country")
 
+        self.address = lambda x: extractElem(self.lookFor(x, "address"))
 
-    def request(self,URL):
+        self.creation_date = lambda x: extractElem(
+            self.lookFor(x, "creation_date"), shouldBe="datetime")
+        self.updated_date = lambda x: extractElem(
+            self.lookFor(x, "updated_date"), shouldBe="datetime")
+        self.expiration_date = lambda x: extractElem(
+            self.lookFor(x, "expiration_date"), shouldBe="datetime")
+
+    def request(self, URL):
         try:
             response = {}
             time.sleep(0.01)
             response = whois.whois(URL)
             self.cache[URL] = response
             return True
-        except Exception as e: 
+        except Exception as e:
             print(f"(WhoIsManager request): {e}")
             return False
 
     def lookFor(self, URL, field=""):
-        #effettua la richiesta se non è già in cache
+        # effettua la richiesta se non è già in cache
         if URL not in self.cache:
-            if not self.request(URL): return None
-        
-        if field=="":
+            if not self.request(URL):
+                return None
+
+        if field == "":
             return self.cache[URL]
 
-        #controlla se il field richiesto è presente
+        # controlla se il field richiesto è presente
         if field in self.cache[URL]:
             return self.cache[URL][field]
         return None
@@ -75,10 +83,10 @@ class whoIsManager:
         dict = {}
         for field in whoIsManager.relevant_fields:
             try:
-                dict[field] = str(getattr(self,field)(URL))
+                dict[field] = str(getattr(self, field)(URL))
             except:
                 dict[field] = "aa"
-        return json.dumps(dict)
+        return json.dumps(dict, indent=4)
 
 
 def main():
@@ -91,6 +99,8 @@ def main():
         # print(whoIs.udf_creation_date(url))
         # print(type(whoIs.udf_creation_date(url)))
         print("-"*30)
+
+
     # print(whoIs.lookFor("https://github.com","domain_name"))
     # print(whoIs.lookFor("https://wikipedia.com","domain_name"))
     # print(whoIs.lookFor("https://lynn-kwong.medium.com/","domain_name"))
